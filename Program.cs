77f50f03a674f47c7263c375cc020bf324391214
Pradeep <deeputhecruz@gmail.com>
2026-04-12 23:59:57 -0500

    new learning message

--- Program.cs
+++ Program.cs
@@ -6,5 +6,6 @@ class Program
     {
         Console.WriteLine("Hello, Git!");
         Console.WriteLine("This is my first C# project.");
+        Console.WriteLine("I am learning how to use Git for version control.");
     }
 }
